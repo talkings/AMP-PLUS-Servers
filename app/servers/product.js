0613b7e20d69c35abf63178c9f95a773f9ee33d6
qianxiang.fang <821249752@qq.com>
2017-11-21 15:33:23 +0800

    增加查询项目信息接口

--- app/servers/product.js
+++ app/servers/product.js
@@ -1,10 +1,10 @@
 module.exports = function( app ){
+    const { mysql } = app.model;
     return {
         /**
          * 创建mock项目 非自动事物处理
          */
         async addMockProduct( ctx, params ) {
-            const { mysql } = app.model;
             return new Promise((resolve, reject)=> {
                 mysql.sequelize.transaction().then((t) => {
                     //查询在用户下是否存在相同的项目名称
@@ -50,7 +50,22 @@ module.exports = function( app ){
          * 获取mock项目信息
          */
         async getMockProduct(ctx, params) {
-            
+            //跳过 x 条数据并获取其后的 x 条数据
+            const current = Number(params.current);
+            //每页显示多少条
+            const pagenum = Number(params.pagenum);
+            return mysql.product_info.findAndCountAll({
+                    offset: current * pagenum - pagenum,
+                    limit: pagenum,
+                    order: [
+                        ['updated_at', 'DESC']
+                    ],
+                    where: {
+                        $and: [{
+                            originator_userid: params.userid
+                        }]
+                    }
+                });
         }
     };
 };
